be77558a4dc35aa8c97dd33314911e5f6714ddff
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-07-15 17:10:41 +0200

    created user details

--- registration/src/main/java/com/registration/model/User.java
+++ registration/src/main/java/com/registration/model/User.java
@@ -1,4 +1,12 @@
 package com.registration.model;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class User {
+    private Long id;
+    private String name;
+    private String surname;
+    private String password;
+    private String email;
 }
