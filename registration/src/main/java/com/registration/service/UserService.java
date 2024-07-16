afc0c82c3944deb0aa75512e804384e4f818edfe
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-07-16 15:48:01 +0200

    updated user service register

--- registration/src/main/java/com/registration/service/UserService.java
+++ registration/src/main/java/com/registration/service/UserService.java
@@ -3,7 +3,9 @@ package com.registration.service;
 import com.registration.model.User;
 import com.registration.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
