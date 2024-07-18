6353d80a1891da178afbcfdc01dfb4360a9a4eba
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-07-18 12:12:56 +0200

    updated login service controller inports

--- login/src/main/java/com/login/controller/LoginController.java
+++ login/src/main/java/com/login/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.login.controller;
 
 import com.login.model.User;
+import com.login.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
