da974a56056e9a1c1eae8740ed4f1a81e7d14919
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-07-22 20:12:57 +0200

    updated loginrequest lombok getter and setter

--- login/src/main/java/com/login/dto/LoginRequest.java
+++ login/src/main/java/com/login/dto/LoginRequest.java
@@ -1,22 +1,12 @@
 package com.login.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class LoginRequest {
     private String username;
     private String password;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
