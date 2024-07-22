73fbba212e26c495b42811d5c8e4e34a3172d3a4
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-07-22 20:07:35 +0200

    created Login Request DTO

---
+++ login/src/main/java/com/login/dto/LoginRequest.java
@@ -0,0 +1,22 @@
+package com.login.dto;
+
+public class LoginRequest {
+    private String username;
+    private String password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
