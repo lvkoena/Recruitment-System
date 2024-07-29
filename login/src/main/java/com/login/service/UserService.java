3f89e7529c95fe252610ec1ce1d2b4be38b879b6
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-07-29 11:51:58 +0200

    updated user service file

--- login/src/main/java/com/login/service/UserService.java
+++ login/src/main/java/com/login/service/UserService.java
@@ -4,18 +4,29 @@ import com.login.model.User;
 import com.login.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class UserService {
     @Autowired
-
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private static final String REGISTRATION_SERVICE_URL = "http://localhost:8080/register";
 
     public User login(String username, String password) {
         User user = userRepository.findByUsername(username);
 
         if (user == null) {
-            throw new RuntimeException("Invalid username. User not found!");
+            // Fetch user from the registration service
+            user = fetchUserFromRegistrationService(username);
+            if (user == null) {
+                throw new RuntimeException("Invalid username. User not found!");
+            }
+            // Save the user to the login service's database
+            userRepository.save(user);
         }
 
         if (!user.getPassword().equals(password)) {
@@ -23,5 +34,13 @@ public class UserService {
         }
 
         return user;
+    }
+
+    private User fetchUserFromRegistrationService(String username) {
+        try {
+            return restTemplate.getForObject(REGISTRATION_SERVICE_URL + "/by-username/" + username, User.class);
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
